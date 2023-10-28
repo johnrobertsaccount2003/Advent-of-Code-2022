ed80ccac52bcf279eeb874a4f24c81a109b6b35c
John Roberts <jdr175@student.bham.ac.uk>
2023-10-28 22:09:19 +0100

    Day 2 and 3

--- Day 2/Day 2 Program.py
+++ Day 2/Day 2 Program.py
@@ -7,12 +7,54 @@ f.close()
 
 #Day 1
 
+games = fileinput.splitlines()
+
+score = 0
+
+for i in games: #Loops through all the games
+    moves = i.split(" ")
+    score += (ord(moves[1]) - 87) #X is 88, Y is 89, Z is 90. This gives the correct score for the move you pick
+
+    #These 2 lines give each player a zero if they play rock, 1 for paper and 2 for scissors
+    opponent = ord(moves[0]) - 65 #A is 65, B is 66, C is 67
+
+    you = ord(moves[1]) - 88
 
 
 
+    if opponent == you:
+        score += 3 #3 points for a draw
 
+
+    #If you have one ascii value higher than the opponent (or 2 for scissors vs 0 against rock), you win and get 6 points
+    elif (you - opponent) % 3 == 1:
+        score += 6
+    
+
+print(score)
 
 #Day 2
+
+
+score = 0
+
+
+for i in games:
+    moves = i.split(" ")
+
+    if moves[1] == "Y":
+        score += 3 + ord(moves[0]) - 64
+
+    elif moves[1] == "X":
+        score += ((ord(moves[0]) - 65 - 1) % 3) + 1
+
+    else:
+        score += 6 + ((ord(moves[0]) - 65 + 1) % 3) + 1
+
+
+print(score)
+
+
 
 
 
--- Day 3/Day 3 Program.py
+++ Day 3/Day 3 Program.py
@@ -7,8 +7,24 @@ f.close()
 
 #Day 1
 
+rucksacks = fileinput.splitlines()
 
+total = 0
 
+for i in rucksacks:
+    half_point = len(i) // 2
+
+    first_compartment = i[:half_point]
+    second_compartment = i[half_point:]
+
+    copy = list(set(first_compartment).intersection(set(second_compartment)))[0]
+
+    if ord(copy) < 93:
+        total += ord(copy) - 38 #ord(A) is 65 so this makes it add 27 for A to 52 for Z
+
+    else:
+        total += ord(copy) - 96 #ord(a) is 97 so this makes it add 1 for a to 26 for z
+print(total)
 
 
 
@@ -16,5 +32,19 @@ f.close()
 
 
 
+
+total = 0
+
+for i in range(0, len(rucksacks), 3):
+    copy = list(set(rucksacks[i]).intersection(set(rucksacks[i+1]).intersection(set(rucksacks[i+2]))))[0]
+
+    if ord(copy) < 93:
+        total += ord(copy) - 38 #ord(A) is 65 so this makes it add 27 for A to 52 for Z
+
+    else:
+        total += ord(copy) - 96 #ord(a) is 97 so this makes it add 1 for a to 26 for z
+
+
+print(total)
 
 
