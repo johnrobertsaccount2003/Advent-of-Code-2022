f700390fe261ff34c21a249cf877fe5d403af5ab
John Roberts <jdr175@student.bham.ac.uk>
2023-11-01 20:39:46 +0000

    .\gitupdate.py

--- Day 21/Day 21 Program.py
+++ Day 21/Day 21 Program.py
@@ -1,3 +1,5 @@
+from sympy import *
+
 f = open("Day 21 Input.txt", "r")
 
 fileinput = f.read()
@@ -7,14 +9,85 @@ f.close()
 
 #Day 1
 
+monkeys = fileinput.splitlines()
 
+instructions = {}
 
+finished = {}
+
+for i in monkeys:
+    temp = i.split(" ")
+    if i[6] not in map(str, range(10)):
+        equation = """finished[\"""" + temp[1] + "\"]" + temp[2] + "finished[\"" + temp[3] + "\"]"
+    else:
+        equation = i.split(" ")[1]
+        finished[temp[0][:-1]] = equation
+
+    instructions[temp[0][:-1]] = equation
+
+while True:
+    for i in monkeys:
+        temp = i.split(" ")
+        temp[0] = temp[0][:-1]
+        try:
+            finished[temp[0]] = eval(instructions[temp[0]])
+        except Exception as e:
+            #print(instructions[temp[0]])
+            #print(e)
+            pass
+    if "root" in finished:
+        print(int(finished["root"]))
+        break
 
 
 
 #Day 2
 
+monkeys = fileinput.splitlines()
 
+instructions = {}
+
+finished = {}
+
+for i in monkeys:
+    temp = i.split(" ")
+    if "root" in temp[0]:
+        conjoined = ""
+        for j in temp[2]:
+            if j not in ("+-/*"):
+                conjoined += j
+            else:
+                conjoined += "="
+        temp[2] = conjoined
+    #print(temp)
+    if "humn" in temp[0]:
+        temp[1] = "x"
+        x = symbols("x")
+        #print(temp)
+    if i[6] not in map(str, range(10)):
+        equation = """finished[\"""" + temp[1] + "\"] " + temp[2] + " finished[\"" + temp[3] + "\"]"
+    else:
+        equation = temp[1]
+        finished[temp[0][:-1]] = equation
+
+    instructions[temp[0][:-1]] = equation
+
+while True:
+    for i in monkeys:
+        temp = i.split(" ")
+        temp[0] = temp[0][:-1]
+        try:
+            get = instructions[temp[0]]
+            bits = get.split(" ")
+            finished[temp[0]] = "(%s)%s(%s)" % (eval(bits[0]), bits[1], eval(bits[2]))
+        except Exception as e:
+            pass
+    if "root" in finished:
+        parts = finished["root"].split("=")
+        answer = eval("solveset(Eq(%s, %s), x)" % (parts[0], parts[1]))
+        for i in answer:
+            print(int(i))
+        break
 
 
 
